a5792a80d23e4f4cbc639d94abba928cba1851a9
aamoskekkonen <aamoskekkonen@icloud.com>
2023-03-20 00:03:32 +0200

    Fix ContentView

--- Kitchen-Surfing/Main/ContentView.swift
+++ Kitchen-Surfing/Main/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("This is a test!")
-        }
-        .padding()
+        HomeView()
     }
 }
 
