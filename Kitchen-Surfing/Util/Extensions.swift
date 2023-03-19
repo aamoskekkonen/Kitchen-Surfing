c302acc222fef0113b43bd2690728bc2d23c3c5d
aamoskekkonen <aamoskekkonen@icloud.com>
2023-03-19 23:42:45 +0200

    Specify app colors

---
+++ Kitchen-Surfing/Util/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  Kitchen-Surfing
+//
+//  Created by Aamos Kekkonen on 19.3.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color(red: 242/255, green: 246/255, blue: 249/255)
+    static let text = Color(red: 30/255, green: 30/255, blue: 30/255)
+    static let greenText = Color(red: 130/255, green: 141/255, blue: 109/255)
+    static let icon1 = Color(red: 85/255, green: 85/255, blue: 73/255)
+    static let icon2 = Color(red: 71/255, green: 67/255, blue: 65/255)
+    static let green1 = Color(red: 212/255, green: 232/255, blue: 178/255)
+    static let green2 = Color(red: 188/255, green: 223/255, blue: 149/255)
+    static let green3 = Color(red: 108/255, green: 195/255, blue: 49/255)
+    static let pink1 = Color(red: 245/255, green: 220/255, blue: 223/255)
+    static let red1 = Color(red: 251/255, green: 103/255, blue: 101/255)
+    static let beige = Color(red: 235/255, green: 224/255, blue: 217/255)
+}
