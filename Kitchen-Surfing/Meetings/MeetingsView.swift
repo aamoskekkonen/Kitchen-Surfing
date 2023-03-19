b486a5556ba52528e3671c672c7734d57846f2dd
aamoskekkonen <aamoskekkonen@icloud.com>
2023-03-20 00:11:25 +0200

    Create MeetingsView

---
+++ Kitchen-Surfing/Meetings/MeetingsView.swift
@@ -0,0 +1,26 @@
+//
+//  MeetingsView.swift
+//  Kitchen-Surfing
+//
+//  Created by Aamos Kekkonen on 20.3.2023.
+//
+
+import SwiftUI
+
+struct MeetingsView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("PAYABLE")
+                
+                MeetingList()
+            }
+        }
+    }
+}
+
+struct MeetingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingsView()
+    }
+}
