2ecfa3b4313fdbd040cebd5fd690c18dc034b3da
aamoskekkonen <aamoskekkonen@icloud.com>
2023-03-19 23:25:10 +0200

    Initial Commit

---
+++ Kitchen-Surfing/Kitchen_SurfingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kitchen_SurfingApp.swift
+//  Kitchen-Surfing
+//
+//  Created by Aamos Kekkonen on 19.3.2023.
+//
+
+import SwiftUI
+
+@main
+struct Kitchen_SurfingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
